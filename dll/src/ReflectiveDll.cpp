5d19ce4e0667b4a7238933ae9ec505daab88f8fd
pegmatite <fmorisato@gmail.com>
2023-06-21 00:03:44 +0900

    Succeeded to run hooking code in the target process but SwapChain::Present does not seems to be called

--- dll/src/ReflectiveDll.cpp
+++ dll/src/ReflectiveDll.cpp
@@ -5,7 +5,21 @@
 #include "hook.h"
 #include "base-window.h"
 #include "d3d11-base-helper.h"
+#include <iostream>
+#include <string>
 #include <polyhook2\Detour\x64Detour.hpp>
+#include <MinHook.h>
+#pragma comment(lib, "libMinHook.x64.lib")
+
+#ifndef BUILD_CONFIG
+#define BUILD_CONFIG "RelWithDebInfo"
+#endif
+
+constexpr int c_strcmp(char const* lhs, char const* rhs) {
+	return (('\0' == lhs[0]) && ('\0' == rhs[0])) ? 0
+		: (lhs[0] != rhs[0]) ? (lhs[0] - rhs[0])
+		: c_strcmp(lhs + 1, rhs + 1);
+}
 // Note: REFLECTIVEDLLINJECTION_VIA_LOADREMOTELIBRARYR and REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN are
 // defined in the project properties (Properties->C++->Preprocessor) so as we can specify our own 
 // DllMain and use the LoadRemoteLibraryR() API to inject this DLL.
@@ -15,6 +29,43 @@ extern HINSTANCE hAppInstance;
 //===============================================================================================//
 
 typedef HRESULT(WINAPI* D3D11PresentPointer)(IDXGISwapChain* swapChain, UINT syncInterval, UINT flags);
+
+LPVOID swapchain_present_vtable_lookup() {
+	// Credits: https://www.unknowncheats.me/forum/d3d-tutorials-and-source/88369-universal-d3d11-hook.html
+	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
+	ID3D11Device* pDevice = nullptr;
+	ID3D11DeviceContext* pContext = nullptr;
+	IDXGISwapChain* pSwapChain = nullptr;
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.OutputWindow = GetForegroundWindow();
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.Windowed = TRUE;
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	if (FAILED(D3D11CreateDeviceAndSwapChain(
+		NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, &featureLevel, 1,
+		D3D11_SDK_VERSION, &swapChainDesc, &pSwapChain, &pDevice, NULL, &pContext))) {
+		std::cout << "D3D11CreateDeviceAndSwapChain failed" << std::endl;
+		return nullptr;
+	}
+
+	DWORD_PTR* pSwapChainVtable;
+	pSwapChainVtable = (DWORD_PTR*)pSwapChain;
+	pSwapChainVtable = (DWORD_PTR*)pSwapChainVtable[0];
+	LPVOID ret = reinterpret_cast<LPVOID>(pSwapChainVtable[8]);
+
+	pDevice->Release();
+	pContext->Release();
+	pSwapChain->Release();
+
+	return ret;
+}
 
 D3D11PresentHook* D3D11PresentHook::Get() {
 	static D3D11PresentHook hook;
@@ -51,6 +102,9 @@ HRESULT D3D11PresentHook::Hook() {
 	if (FAILED(hr)) {
 		return hr;
 	}
+	////DWORD_PTR hDxgi = (DWORD_PTR)GetModuleHandle("dxgi.dll");
+	////std::uint64_t presentPointer_ = reinterpret_cast<std::uint64_t>(swapchain_present_vtable_lookup());
+
 	// Pointer to the swap chain.
 	std::uintptr_t* swapChainPointer = static_cast<std::uintptr_t*>(static_cast<void*>(d3d11Helper.GetSwapChain()));
 	// Pointer to the swap chain virtual table.
@@ -68,9 +122,12 @@ HRESULT D3D11PresentHook::Hook() {
 	// This will be called instead of the original "Present".
 	std::uint64_t presentCallback = reinterpret_cast<std::uint64_t>(&D3D11PresentHook::SwapChainPresentWrapper);
 
-	PLH::x64Detour* detour = new PLH::x64Detour(presentPointer_, presentCallback, &presentTrampoline_);
-	// Hook!
-	detour->hook();
+	std::cout << "Start hooking" << std::endl;
+	MH_Initialize();
+	MH_CreateHook(&presentPointer_, &presentCallback, reinterpret_cast<LPVOID*>(&presentTrampoline_));
+	MH_EnableHook(&presentPointer_);
+	std::cout << "Done hooking" << std::endl;
+
 	return S_OK;
 }
 
@@ -93,10 +150,18 @@ HRESULT D3D11PresentHook::SwapChainPresent(IDXGISwapChain* swapChain, UINT syncI
 }
 
 HRESULT D3D11PresentHook::SwapChainPresentWrapper(IDXGISwapChain* swapChain, UINT syncInterval, UINT flags) {
+	std::cout << "D3D11PresentHook::SwapChainPresentWrapper called" << std::endl;
 	return D3D11PresentHook::Get()->SwapChainPresent(swapChain, syncInterval, flags);
 }
 
 DWORD  WINAPI run_thread(LPVOID param) {
+	if constexpr (c_strcmp(BUILD_CONFIG, "RelWithDebInfo") == 0) {
+		AllocConsole();
+		SetConsoleTitle("Sekiro Practice Tool DLL by johndisandonato");
+		freopen("CONOUT$", "w", stdout);
+		freopen("CONOUT$", "w", stderr);
+		freopen("CONIN$", "r", stdin);
+	}
 	MessageBoxA(NULL, "Dll injecting...", "Reflective Dll Injection", MB_OK);
 	HRESULT hr;
 	hr = D3D11PresentHook::Get()->Hook();
